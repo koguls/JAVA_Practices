879230ffe1ed2aa385841897f21611884395c80c
koguls <kogulkogul4@gmail.com>
2024-07-11 12:22:27 +0530

    day3

---
+++ random.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class random{
+    public static void main(String args[] ){
+
+        Random ran = new Random();
+
+        int a = ran.nextInt();
+        double b = ran.nextDouble();
+        boolean c = ran.nextBoolean();
+
+        System.out.print();
+
+    }
+}
