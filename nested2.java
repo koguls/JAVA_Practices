e9b6dfa616a4b91e4d87ca15d3bafd72ac3fc727
KOGUL <134825692+koguls@users.noreply.github.com>
2024-07-12 22:28:09 +0530

    nested2.java

---
+++ nested2.java
@@ -0,0 +1,8 @@
+pubilc class nested2{
+  pubilc static void main(String args[]){
+    for(int i = 0; i<3; i++){
+      for(int j=o; j<3; j++){
+        System.out.print(i+" "+j);
+      }
+    }
+  }}
