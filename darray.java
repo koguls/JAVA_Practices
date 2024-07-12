649c9779786efba994e93d5a878c73dba4b6a013
koguls <kogulkogul4@gmail.com>
2024-07-12 22:11:17 +0530

    day4

---
+++ darray.java
@@ -0,0 +1,14 @@
+public class darray{
+public static void main(String arg[]){
+    String[][] arrays  = new String[3][2];
+
+    arrays[0][0]="cars";
+    arrays[0][1]="tesla";
+
+    String[][] a = {{"carsss","teslasss","tesala","tesla"},{"carsss","teslasss"},{"carsss","teslasss"}};
+
+
+    System.out.println(a[0][1]); 
+    System.out.print(arrays[0][1]);
+}
+}
