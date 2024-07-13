040555af3dcdab7781e4a8171c6fec5b60075e3a
koguls <kogulkogul4@gmail.com>
2024-07-13 13:50:34 +0530

    day5

---
+++ cars.java
@@ -0,0 +1,29 @@
+public  class cars {
+
+    String make = "india";
+    String model = "tata ev";
+    int year = 2024;
+    String color = "white";
+    double price = 50000.00;
+
+
+    void drive(){
+        System.out.println("you drive a car");
+
+        
+    }
+
+    void brake(){
+        System.out.println("you step on brake");
+    }
+
+    void details(){
+        System.out.println(make);
+        System.out.println(model);
+        System.out.println(year);
+        System.out.println(color);
+        System.out.println(price);
+    }
+}
+    
+
