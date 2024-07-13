43ba3d9a9f0db62bb8486a26d68baa39ced564bf
koguls <kogulkogul4@gmail.com>
2024-07-13 13:43:42 +0530

    day1

---
+++ oops.java
@@ -0,0 +1,8 @@
+public class oops {
+    public static void main(String[] args) {
+       cars mycar = new cars();
+       System.out.println(mycar.make); 
+       System.out.println(mycar);
+       mycar.details();
+    }
+}
