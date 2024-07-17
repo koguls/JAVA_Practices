b3547df568b300a49528196c57e902ce404b355e
koguls <kogulkogul4@gmail.com>
2024-07-17 15:26:04 +0530

    overloaded constructors

---
+++ pizza.java
@@ -0,0 +1,45 @@
+import javax.print.DocFlavor.STRING;
+
+public class pizza {
+
+    String Bread;
+    String sauce;
+    String cheese;
+    String topping;
+
+    pizza(){
+        System.out.println("no pizza");
+    }
+
+    pizza(String bread,String sauce,String cheese,String topping){
+
+        this.Bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+
+
+    }
+    
+    pizza(String bread,String sauce,String cheese,){
+
+        this.Bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        
+
+
+    }
+    pizza(String bread,String sauce){
+
+        this.Bread = bread;
+        this.sauce = sauce;
+       
+    }
+    pizza(String bread){
+
+        this.Bread = bread;
+    
+    }
+}
+    
