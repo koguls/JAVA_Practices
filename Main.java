a0507d2122c0273f42307cd9e76c3df5a512fc3c
koguls <kogulkogul4@gmail.com>
2024-07-17 15:38:14 +0530

    main file

--- Main.java
+++ Main.java
@@ -2,11 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        localgobal Local = new localgobal();
+        localgobal local = new localgobal();
+        System.out.println(local);
 
-        pizza pizza = new pizza();
 
-        pizza("pizza");
+        
+        pizza Pizza = new pizza("NORMAL bread");
+       
+
+        System.out.println("here are ingredients");
+        System.out.println(Pizza.Bread);
+        System.out.println(Pizza.sauce);
+        System.out.println(Pizza.cheese);
+        System.out.println(Pizza.topping);
+
+
+        
         
 
         
