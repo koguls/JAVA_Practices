3cb9a44d1555febd6402d1b0d7bc9cfcd1a6b02e
koguls <kogulkogul4@gmail.com>
2024-07-17 15:21:31 +0530

    main file of java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        localgobal Local = new localgobal();
+
+        pizza pizza = new pizza();
+
+        pizza("pizza");
+        
+
+        
+        
+    }
+    
+}
