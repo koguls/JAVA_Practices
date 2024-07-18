e2b52f498583060e70e5746e376971871d7c94bd
koguls <kogulkogul4@gmail.com>
2024-07-18 11:54:07 +0530

    interface

--- hawk.java
+++ hawk.java
@@ -1,3 +1,8 @@
-public class hawk {
+public class hawk implements predator{
+
+    public void hunt(){
+        System.out.println(" hawk is class \n with include interface predator file \n in @override ");
+    }
+
     
 }
