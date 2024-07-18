1c975fea55e16c7167420ad758cd70376b7c5fff
koguls <kogulkogul4@gmail.com>
2024-07-18 11:54:36 +0530

    interface

--- main41.java
+++ main41.java
@@ -3,6 +3,11 @@ public class main41 {
     public static void main(String[] args) {
         rabbit ra = new rabbit();
         ra.flee();
+
+        System.out.println();
+
+        hawk ha = new hawk();
+        ha.hunt();
     }
     
 }
