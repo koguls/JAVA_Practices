4e635ced61aaee8b97465dd748ef82b3c6455e7b
koguls <kogulkogul4@gmail.com>
2024-07-18 11:42:41 +0530

    interface class

---
+++ main41.java
@@ -0,0 +1,8 @@
+public class main41 {
+
+    public static void main(String[] args) {
+        rabbit ra = new rabbit();
+        ra.flee();
+    }
+    
+}
---
+++ rabbit.java
@@ -0,0 +1,7 @@
+public class rabbit implements prey {
+
+   public void flee(){
+    System.out.println("rabbit is class to used interface preyfile in override method ");
+   }
+    
+}
