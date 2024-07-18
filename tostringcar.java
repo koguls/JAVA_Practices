38c4158a27fb7abef992b93e2624f0d5f08dbded
koguls <kogulkogul4@gmail.com>
2024-07-18 12:21:06 +0530

    tostring

---
+++ tostringcar.java
@@ -0,0 +1,13 @@
+public class tostringcar {
+    String make = "ford";
+    String model= "mustang";
+    String color = "red";
+    int year = 2023;
+
+    public String toString(){
+        return make +"\n"+ model+"\n"+color+"\n"+year; 
+    }
+
+
+    
+}
