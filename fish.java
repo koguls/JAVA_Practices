b0b53bfd5dbc9c164bbce7f31b4380c6d735e2dd
koguls <kogulkogul4@gmail.com>
2024-07-18 12:00:52 +0530

    interface 2 implements

--- fish.java
+++ fish.java
@@ -1,4 +1,11 @@
-public class fish {
+public class fish implements predator,prey {
+
+    public void hunt(){
+        System.out.println("fish is a class with a interface implementation used override");
+    }
+    public void flee(){
+        System.out.println("to used a 2 interfaces file on the fish one class ");
+    }
 
     
 }
