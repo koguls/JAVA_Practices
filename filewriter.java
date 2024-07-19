8017729bcc51de16df8bfa814a0967faf68a67e3
koguls <kogulkogul4@gmail.com>
2024-07-19 10:43:24 +0530

    write

--- filewriter.java
+++ filewriter.java
@@ -1,5 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class filewriter {
 
-    
+    public static void main(String[] args) {
+
+       try{
+            FileWriter writer = new FileWriter("poem.txt");
+            writer.write("hello" + 123 + "\nroses are red \nviolet are blue \n booty booty winner winner chicken dinnner");
+            writer.close();
+       }
+       catch(IOException e){
+
+        System.out.println("erorr");
+
+       }
+    }
     
 }
