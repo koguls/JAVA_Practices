b29f675cdbe6376055d6de7afc39a69e813486bb
koguls <kogulkogul4@gmail.com>
2024-07-19 10:07:29 +0530

    io file

--- file.java
+++ file.java
@@ -1,3 +1,40 @@
+import java.io.File;;
+
 public class file {
+
+    public static void main(String[] args) {
+
+
+        File file = new File("textfile.txt");
+
+        while (file.exists()) {
+            
+        
+
+        if(file.exists()){
+            System.out.println("this file is there :o!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            System.out.println(file.isHidden());
+            System.out.println(file.isDirectory());
+            file.delete();
     
+        }
+        
+        
+        else{
+            System.out.println("the file doest exists:(");
+        }
+    }
+    if(!file.exists()){
+        System.out.println("this deleted successfuly");
+
+        
+    }
+
+    
+
+    
+    }
 }
