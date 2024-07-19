6946b1a9ab02da3aa980e09a0d4ef544845f20ea
koguls <kogulkogul4@gmail.com>
2024-07-19 11:04:57 +0530

    reader

--- filereader.java
+++ filereader.java
@@ -1,3 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.sound.midi.Soundbank;
+
 public class filereader {
+
+    public static void main(String[] args) {
+
+        try{
+        FileReader reads = new FileReader("poem.txt");
+        int data = reads.read();
+    
+        while(data != -1) {
+
+            System.out.print((char)data);
+            data =reads.read();
+            
+        }
+        reads.close();
+        }
+        catch(FileNotFoundException e){
+
+            e.printStackTrace();
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+    }
     
 }
