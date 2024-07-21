3c1df3d4fddd30e3015d613a8b32d5e960b23d73
koguls <kogulkogul4@gmail.com>
2024-07-21 11:38:18 +0530

    javafx

---
+++ jframe.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.color.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class jframe {
+
+    public static void main(String[] args) {
+         
+        JFrame frame = new JFrame();
+        frame.setTitle("my java jframe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+        
+
+
+        ImageIcon image = new ImageIcon("ko.png");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(Color.green);
+    }
+}
