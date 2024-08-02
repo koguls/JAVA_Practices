b22507efa0046f2f8676b46c399c2cc01eff7b34
KOGUL <134825692+koguls@users.noreply.github.com>
2024-08-02 12:24:25 +0530

    Create SWapsingle.java

---
+++ SWapsingle.java
@@ -0,0 +1,12 @@
+class SWapsingle {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+       
+                     //a=b a is 20
+        b=a+b - (a=b);
+        
+        
+        System.out.print("a is "+a+"\nb is"+b);
+    }
+}
