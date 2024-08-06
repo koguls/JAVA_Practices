f53086936957271016d7502c20a29628eab88c15
KOGUL <134825692+koguls@users.noreply.github.com>
2024-08-06 09:31:51 +0530

    Create Tester4.java

---
+++ Tester4.java
@@ -0,0 +1,9 @@
+class Tester4 {
+	public static void main(String[] args) {
+		// Implement your code here 
+		float pi = 3.24f;
+		int radius = 4;
+		Double Area = pi*radius*radius;
+		System.out.println("Area: "+ Area);
+	}
+}
