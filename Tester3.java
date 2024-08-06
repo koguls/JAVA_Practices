442b67855885da5b2958cb7c34d6b6237469ce14
KOGUL <134825692+koguls@users.noreply.github.com>
2024-08-06 09:25:48 +0530

    Create Tester3.java

---
+++ Tester3.java
@@ -0,0 +1,11 @@
+class Tester3 {
+	public static void main(String[] args) {
+		// Implement your code here
+		int principal=5000;
+		int rate=10;
+		int time=5;
+		double simple_interest=(principal*rate*time)/100;
+		System.out.println("Simple Interest: "+ simple_interest);
+	}
+}
+
