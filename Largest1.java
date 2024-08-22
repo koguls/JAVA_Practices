608ce8141ce548b3ee796504bbac4ee0af40a42e
KOGUL <134825692+koguls@users.noreply.github.com>
2024-08-22 12:36:38 +0530

    Create Largest1.java

---
+++ Largest1.java
@@ -0,0 +1,33 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Largest1 {
+    public static void main(String[] args) {
+        double num1 = 10, num2 =20, num3 = 30;
+        
+        if(num1>num2 && num1>num3){
+            System.out.println("num1 is largest number"  );
+        }else if (num2>num1 && num2>num3){
+            System.out.println("num2 is largest number");
+        }else{
+            System.out.println("num3 is largest number");
+        }
+    
+    }
+    class Largest2 {
+        public void Largest2(int num1, int num2, int num3){
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        
+        public void large(int this.num1,int this.num2,int this.num3){
+            if(num1>num2){
+                if(num1>num3){
+                   System.out.println("num1 is largest number"  ); 
+                }
+             
+            }
+        }
+    }
+}
